5f72c2e91cc309002106628a61b5cb724c51fcb5
Drozdov Evgeny <drozdove912@gmail.com>
2025-04-10 12:15:32 +0200

    3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,15 @@
   <AppDiv title="Home">
     <div class="inf">
       <div class="ff">
-        <h2>Информаци</h2>
+        <div class="item">
+          <h2>Информаци</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+            assumenda nesciunt doloribus reprehenderit eos adipisci esse ea
+            voluptate? Odio similique commodi distinctio quis, neque rem eaque
+            quod ea ratione fugiat.
+          </p>
+        </div>
       </div>
       <div class="ff">
         <img src="../../public/2.jpg" />
@@ -25,8 +33,14 @@ import AppDiv from "@/components/AppDiv.vue";
   display: flex;
   gap: 10px;
 }
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .ff {
   display: flex;
+  flex-direction: column;
   align-items: center;
   gap: 20px;
   width: 100%;

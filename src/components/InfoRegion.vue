ba8def4846f81f38fa6038b61c67c4546116f260
Drozdov Evgeny <drozdove912@gmail.com>
2025-04-18 17:59:38 +0200

    pagination

--- src/components/InfoRegion.vue
+++ src/components/InfoRegion.vue
@@ -11,55 +11,20 @@
         <p>{{ key }}</p>
       </div>
     </div>
-    <div class="pagination">
-      <button
-        @click="curentPage > 1 && curentPage--"
-        :disabled="curentPage === 1"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="var(--act)"
-        >
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
-        </svg>
-      </button>
-      <button
-        v-for="page in totalPage"
-        :key="page"
-        @click="curentPage = page"
-        :class="{ active: curentPage === page }"
-      >
-        {{ page }}
-      </button>
-      <button
-        @click="curentPage < totalPage && curentPage++"
-        :disabled="curentPage === totalPage"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          enable-background="new 0 0 24 24"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="var(--act)"
-        >
-          <g><path d="M0,0h24v24H0V0z" fill="none" /></g>
-          <g>
-            <polygon points="6.23,20.23 8,22 18,12 8,2 6.23,3.77 14.46,12" />
-          </g>
-        </svg>
-      </button>
-    </div>
+    <ThePagination
+      :curentPage="curentPage"
+      :totalPage="totalPage"
+      @prev="curentPage > 1 && curentPage--"
+      @next="curentPage < totalPage && curentPage++"
+      @go="(page) => (curentPage = page)"
+    />
   </div>
 </template>
 <script setup>
 import useDataOneRegion from "@/use/UseDataOneRedion";
 import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
+import ThePagination from "./ThePagination.vue";
 const route = useRoute();
 const nameregion = route.params.name;
 const data = computed(() => useDataOneRegion(nameregion));
@@ -79,10 +44,6 @@ const totalPage = computed(() => {
 });
 </script>
 <style scoped>
-.active {
-  color: var(--act);
-  font-weight: 700;
-}
 p {
   padding: 2px;
 }
@@ -118,30 +79,5 @@ p {
   padding: 20px;
   color: var(--content-color);
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-}
-button {
-  background-color: transparent;
-  outline: none;
-  border: none;
-  color: var(--content-color);
-  font-size: 16px;
-}
-svg {
-  transition: all 0.5s ease;
-}
-button:disabled svg {
-  fill: var(--content-color);
-  transform: scale(0.7);
-}
-button:not(:disabled):hover svg {
-  transform: translateX(3px);
-  fill: var(--act-dark); /* Можно задать цвет при наведении */
-}
-
-.pagination {
-  display: flex;
-  gap: 10px;
-  height: 25px;
-  transition: all 0.5s ease;
 }
 </style>
---
+++ src/components/ThePagination.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="pagination">
+    <button @click="$emit('prev')" :disabled="curentPage === 1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="var(--act)"
+      >
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
+      </svg>
+    </button>
+    <button
+      v-for="page in totalPage"
+      :key="page"
+      @click="$emit('go', page)"
+      :class="{ active: curentPage === page }"
+    >
+      {{ page }}
+    </button>
+    <button @click="$emit('next')" :disabled="curentPage === totalPage">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        enable-background="new 0 0 24 24"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="var(--act)"
+      >
+        <g><path d="M0,0h24v24H0V0z" fill="none" /></g>
+        <g>
+          <polygon points="6.23,20.23 8,22 18,12 8,2 6.23,3.77 14.46,12" />
+        </g>
+      </svg>
+    </button>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  totalPage: Number,
+  curentPage: Number,
+});
+const emit = defineEmits(["prev", "next", "go"]);
+</script>
+<style scoped>
+button {
+  background-color: transparent;
+  outline: none;
+  border: none;
+  color: var(--content-color);
+  font-size: 16px;
+}
+svg {
+  transition: all 0.5s ease;
+}
+button:disabled svg {
+  fill: var(--content-color);
+  transform: scale(0.7);
+}
+button:not(:disabled):hover svg {
+  transform: translateX(3px);
+  fill: var(--act-dark);
+}
+
+.pagination {
+  display: flex;
+  gap: 10px;
+  height: 25px;
+  transition: all 0.5s ease;
+}
+.active {
+  color: var(--act);
+  font-weight: 700;
+}
+</style>

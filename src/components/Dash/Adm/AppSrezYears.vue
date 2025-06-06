accbb762e85ed9e07d892018028e5d344d83c873
Drozdov Evgeny <drozdove912@gmail.com>
2025-06-06 18:14:25 +0200

    srezReg

--- src/components/Dash/Adm/AppSrezYears.vue
+++ src/components/Dash/Adm/AppSrezYears.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>Субьект РФ</h3>
-  <Srex :items="items" @year="setYear" />
+  <Srex :items="filteredItems" @year="setYear" />
 </template>
 
 <script setup>
@@ -10,10 +10,25 @@ import useSrezYearStore from "@/store/useSrezYearsStore";
 import Srex from "./Srex.vue";
 import { items } from "../../../utils/region";
 import useSrezRegStore from "@/store/useSrezReg";
+import { storeToRefs } from "pinia";
+import useSrezFoStore from "@/store/useSrezFoStore";
+const { getFo } = storeToRefs(useSrezFoStore());
 const storeReg = useSrezRegStore();
 function setYear(value) {
   storeReg.setReg(value);
 }
+const filteredItems = computed(() => {
+  if (!getFo.value || getFo.value === "Сбросить") {
+    return items.value; // показываем все, если не выбран округ
+  }
+  return items.value.filter((item) => {
+    console.log(item[0]);
+    return (
+      item.fo &&
+      item.fo.trim().toLowerCase() === getFo.value.trim().toLowerCase()
+    );
+  });
+});
 </script>
 <style>
 .active-menu-item > .p-menuitem-link {
